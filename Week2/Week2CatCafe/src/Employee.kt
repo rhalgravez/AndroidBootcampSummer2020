72943bfd06de708558d3944d14f676142fde82e1
Roberto Halgravez <r.halgravez@gmail.com>
2020-06-05 19:06:19 -0600

    Week 2 - Create Employee class (Person subclass)

---
+++ Week2/Week2CatCafe/src/Employee.kt
@@ -0,0 +1,45 @@
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+
+class Employee(
+    name: String,
+    lastName: String,
+    phone: String,
+    email: String,
+    val socialSecurityNumber: String,
+    var hireDate: String,
+    var salary: Double
+) :
+    Person(name, lastName, phone, email) {
+
+    constructor(
+        person: Person,
+        socialSecurityNumber: String,
+        hireDate: String,
+        salary: Double
+    ) :
+            this(person.name, person.lastName, person.phone, person.email, socialSecurityNumber, hireDate, salary)
+
+    fun clockIn() {
+        val stringDate = stringLocalDateTime()
+        println("Employee ${fullName()} check-in was at: $stringDate")
+
+    }
+
+    fun clockOut(isTimeToGoHome: Boolean = false) {
+        if (isTimeToGoHome) {
+            val stringDate = stringLocalDateTime()
+            println("Employee ${fullName()} check-out at: $stringDate")
+        } else {
+            println("Sorry but ${fullName()} still have work to do")
+        }
+    }
+
+    private fun stringLocalDateTime(): String {
+        val current = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("EEEE, MMM dd, yyyy HH:mm:ss a")
+
+        return current.format(formatter)
+    }
+}
