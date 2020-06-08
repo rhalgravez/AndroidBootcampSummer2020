5ad0e4498b8d1226bcf346faec42aeb14443042a
Roberto Halgravez <r.halgravez@gmail.com>
2020-06-08 05:36:25 -0600

    Week 2 -  Create dummy example to see the top ten prints and a receipt example

--- Week2/Week2CatCafe/src/Main.kt
+++ Week2/Week2CatCafe/src/Main.kt
@@ -1,3 +1,23 @@
 fun main() {
+    val cafeController = CafeController()
+
+    println("----------")
+    cafeController.printNumberOfAdoptions()
+    println("----------")
+    cafeController.printUnsponsoredCats()
+    println("----------")
+    cafeController.printSponsoredCats()
+    println("----------")
+    cafeController.printTopTenSellingItems()
+    println("----------")
+    cafeController.printTopTenCats()
+
+    cafeController.doSomeTransactions()
+    println()
+    println("**************")
+    cafeController.printTopTenCats()
+
+    val receipt = cafeController.transactionsList[1]
+    receipt.printTicket()
 
 }
