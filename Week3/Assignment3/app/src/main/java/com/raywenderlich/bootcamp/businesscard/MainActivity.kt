344850579361c524e8ce2568c7dbbcd0f0c1c153
Roberto Halgravez <r.halgravez@gmail.com>
2020-06-12 02:13:22 -0600

    Week 3 - Handling the click events for the menu Handling the click events for the menu and show the AlerDialog
    And just found out about regions

--- Week3/Assignment3/app/src/main/java/com/raywenderlich/bootcamp/businesscard/MainActivity.kt
+++ Week3/Assignment3/app/src/main/java/com/raywenderlich/bootcamp/businesscard/MainActivity.kt
@@ -2,10 +2,40 @@ package com.raywenderlich.bootcamp.businesscard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+    //region Menu
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.actionAbout) {
+            showAlertDialog()
+        }
+        return true
+    }
+
+    private fun showAlertDialog() {
+        val dialogTitle = getString(R.string.alertDialogTitle, BuildConfig.VERSION_NAME)
+        val dialogMessage = getString(R.string.alertDialogMessage)
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(dialogTitle)
+        builder.setMessage(dialogMessage)
+        builder.create().show()
+    }
+    //endregion
+
 }
