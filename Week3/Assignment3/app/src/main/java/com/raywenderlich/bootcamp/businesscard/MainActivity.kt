f48c4d950aa94a9a815a7b56060c6a2a2bb020dc
Roberto Halgravez <r.halgravez@gmail.com>
2020-06-14 15:02:10 -0600

    Week 3 - Load a random quote with an image related to the quote Add the images related to the quotes

--- Week3/Assignment3/app/src/main/java/com/raywenderlich/bootcamp/businesscard/MainActivity.kt
+++ Week3/Assignment3/app/src/main/java/com/raywenderlich/bootcamp/businesscard/MainActivity.kt
@@ -5,13 +5,30 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val quotes = listOf<Pair<Int, String>>(
+        Pair(R.drawable.homer, "You\'ll have to speak up, I'm wearing a towel."),
+        Pair(R.drawable.shinji,"Pilot the eva Shinji or Rei will have to do it again"),
+        Pair(R.drawable.daffy,"Duck season! FIRE!"),
+        Pair(R.drawable.iron_giant,"Superman!!!")
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        randomQuote()
+
     }
 
+    private fun randomQuote() {
+        val (image, randomQuote) = quotes.random()
+        quoteImageView.setImageResource(image)
+        quoteTextView.text = randomQuote
+    }
 
     //region Menu
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
