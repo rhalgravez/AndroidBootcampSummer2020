9574865f52acbec84b58bc16c19f62338a3906ba
Roberto Halgravez <r.halgravez@gmail.com>
2020-06-14 18:36:09 -0600

    Week 3 - Save and restore the quote that is shown when the device rotates

--- Week3/Assignment3/app/src/main/java/com/raywenderlich/bootcamp/businesscard/MainActivity.kt
+++ Week3/Assignment3/app/src/main/java/com/raywenderlich/bootcamp/businesscard/MainActivity.kt
@@ -1,27 +1,38 @@
 package com.raywenderlich.bootcamp.businesscard
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    val quotes = listOf<Pair<Int, String>>(
+    companion object {
+        private const val QUOTE_INDEX_KEY = "QUOTE_INDEX_KEY"
+    }
+
+    private val quotes = listOf(
         Pair(R.drawable.homer, "You\'ll have to speak up, I'm wearing a towel."),
         Pair(R.drawable.shinji,"Pilot the eva Shinji or Rei will have to do it again."),
         Pair(R.drawable.daffy,"Duck season! FIRE!"),
         Pair(R.drawable.iron_giant,"Superman!!!")
     )
 
+    private var currentImageAndQuoteIndex = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        randomQuote()
+        if (savedInstanceState != null) {
+            val savedQuoteIndex = savedInstanceState.getInt(QUOTE_INDEX_KEY)
+            selectQuote(savedQuoteIndex)
+        } else {
+            selectQuote()
+        }
 
         quoteButton.setOnClickListener { view ->
             val bounceAnimation = AnimationUtils.loadAnimation(this, R.anim.bounce)
@@ -30,8 +41,14 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(QUOTE_INDEX_KEY, currentImageAndQuoteIndex)
+    }
+
     private fun selectQuote(index: Int? = null) {
-        val (image, quote) = quotes[index ?: quotes.indices.random()]
+        currentImageAndQuoteIndex = index ?: quotes.indices.random()
+        val (image, quote) = quotes[currentImageAndQuoteIndex]
         quoteImageView.setImageResource(image)
         quoteTextView.text = quote
     }
