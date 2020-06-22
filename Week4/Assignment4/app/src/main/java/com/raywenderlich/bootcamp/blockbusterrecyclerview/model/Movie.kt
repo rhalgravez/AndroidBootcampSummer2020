91381ce53c2faadedb18e5dccdfbfb69e19ec66c
Roberto Halgravez <r.halgravez@gmail.com>
2020-06-22 02:00:04 -0600

    Week 4 - Creta package named "Model" and inside in this package create the class Movie

---
+++ Week4/Assignment4/app/src/main/java/com/raywenderlich/bootcamp/blockbusterrecyclerview/model/Movie.kt
@@ -0,0 +1,14 @@
+package com.raywenderlich.bootcamp.blockbusterrecyclerview.model
+
+import java.util.*
+
+class Movie(
+    val id: String = UUID.randomUUID().toString(),
+    val title: String,
+    val releaseDate: String,
+    val poster: Int,
+    val banner: Int,
+    val rating: String,
+    val summary: String
+) {
+}
