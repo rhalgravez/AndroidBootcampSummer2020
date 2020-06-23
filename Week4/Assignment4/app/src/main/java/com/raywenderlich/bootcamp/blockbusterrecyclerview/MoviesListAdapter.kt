23c59281601c2b14ecec63f2f483e4ed31437f65
Roberto Halgravez <r.halgravez@gmail.com>
2020-06-22 20:19:54 -0600

    Week 4 - Create MoviesListAdapter file that will work as the RecyclerView.Adapter, that will display the ViewHolder that was created previously (MoviesListItemViewHolder)

---
+++ Week4/Assignment4/app/src/main/java/com/raywenderlich/bootcamp/blockbusterrecyclerview/MoviesListAdapter.kt
@@ -0,0 +1,18 @@
+package com.raywenderlich.bootcamp.blockbusterrecyclerview
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MoviesListAdapter: RecyclerView.Adapter<MoviesListItemViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MoviesListItemViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MoviesListItemViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
