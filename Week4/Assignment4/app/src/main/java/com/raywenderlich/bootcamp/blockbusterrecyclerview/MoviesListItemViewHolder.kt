8a6b5b09e8e0857ad0f3b8eb0d75dc8ba04e2c51
Roberto Halgravez <r.halgravez@gmail.com>
2020-06-22 20:03:41 -0600

    Week 4 - Make the MoviesListItemViewHolder a subclass of RecyclerView.VieHolder. Also add reference to imageView and textViews  from the movies_list_item_viewholder.xml file

--- Week4/Assignment4/app/src/main/java/com/raywenderlich/bootcamp/blockbusterrecyclerview/MoviesListItemViewHolder.kt
+++ Week4/Assignment4/app/src/main/java/com/raywenderlich/bootcamp/blockbusterrecyclerview/MoviesListItemViewHolder.kt
@@ -1,4 +1,11 @@
 package com.raywenderlich.bootcamp.blockbusterrecyclerview
 
-class MoviesListItemViewHolder {
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.movies_list_item_viewholder.view.*
+
+class MoviesListItemViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    val movieImageView = itemView.movie_image_view
+    val movieTitleTextView = itemView.title_movie_text_view
+    val movieRatingTextView = itemView.rating_movie_text_view
 }
