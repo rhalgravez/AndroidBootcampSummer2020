8703d68845cd9a9ea7475cd9f68ce23e0b899802
Roberto Halgravez <r.halgravez@gmail.com>
2020-06-22 18:59:38 -0600

    Week 4 - Set the layoutManager for the recycler_view as a GridLayoutManager (to arrange each items in two columns grid)

--- Week4/Assignment4/app/src/main/java/com/raywenderlich/bootcamp/blockbusterrecyclerview/MainActivity.kt
+++ Week4/Assignment4/app/src/main/java/com/raywenderlich/bootcamp/blockbusterrecyclerview/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.raywenderlich.bootcamp.blockbusterrecyclerview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recycler_view.layoutManager = GridLayoutManager(this, 2)
     }
 }
