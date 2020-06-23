132b9c1e40c9ead8fd4bce4d514ea035528e1e04
Roberto Halgravez <r.halgravez@gmail.com>
2020-06-22 20:57:17 -0600

    Week 4 - Modify the MoviesListAdapter contractor to accept an array list of movies so it can load the GridLayout with the real data (the favorites movies) Also send an array list with the MoviesDataSource to the MoviesListAdapter to see if is loading correctly the favorites movies

--- Week4/Assignment4/app/src/main/java/com/raywenderlich/bootcamp/blockbusterrecyclerview/MainActivity.kt
+++ Week4/Assignment4/app/src/main/java/com/raywenderlich/bootcamp/blockbusterrecyclerview/MainActivity.kt
@@ -3,6 +3,7 @@ package com.raywenderlich.bootcamp.blockbusterrecyclerview
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
+import com.raywenderlich.bootcamp.blockbusterrecyclerview.model.MoviesDataSource
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         recycler_view.layoutManager = GridLayoutManager(this, 2)
-        recycler_view.adapter = MoviesListAdapter()
+        recycler_view.adapter = MoviesListAdapter(MoviesDataSource.getMoviesList())
     }
 }
--- Week4/Assignment4/app/src/main/java/com/raywenderlich/bootcamp/blockbusterrecyclerview/MoviesListAdapter.kt
+++ Week4/Assignment4/app/src/main/java/com/raywenderlich/bootcamp/blockbusterrecyclerview/MoviesListAdapter.kt
@@ -3,21 +3,22 @@ package com.raywenderlich.bootcamp.blockbusterrecyclerview
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.raywenderlich.bootcamp.blockbusterrecyclerview.model.Movie
 
-class MoviesListAdapter: RecyclerView.Adapter<MoviesListItemViewHolder>() {
+class MoviesListAdapter(private val moviesList: ArrayList<Movie>): RecyclerView.Adapter<MoviesListItemViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MoviesListItemViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.movies_list_item_viewholder, parent, false)
         return MoviesListItemViewHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        return 8
-    }
+    override fun getItemCount(): Int = moviesList.size
+
 
     override fun onBindViewHolder(holder: MoviesListItemViewHolder, position: Int) {
-        holder.movieImageView.setImageResource(R.drawable.paddington)
-        holder.movieTitleTextView.text = "Paddington".toString()
-        holder.movieRatingTextView.text = "15 / 10".toString()
+        val movie = moviesList[position]
+        holder.movieImageView.setImageResource(movie.poster)
+        holder.movieTitleTextView.text = movie.title
+        holder.movieRatingTextView.text = "${movie.rating} / 10"
     }
 }
