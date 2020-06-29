43d8ac2726b07d01a0b35197e0be4d5d6f501d0b
Roberto Halgravez <r.halgravez@gmail.com>
2020-06-28 22:37:18 -0600

    Week 5 - Add onActivityCreated() method to handle the onClickListener button method and shows a Snackbar when the username has less than 3 characters and password has less than 4 characters

--- Week5/Assignment5/app/src/main/java/com/raywenderlich/bootcamp/blockbusterrecyclerview/LoginFragment.kt
+++ Week5/Assignment5/app/src/main/java/com/raywenderlich/bootcamp/blockbusterrecyclerview/LoginFragment.kt
@@ -1,10 +1,13 @@
 package com.raywenderlich.bootcamp.blockbusterrecyclerview
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.fragment_login.*
 
 
 
@@ -23,5 +26,21 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        button.setOnClickListener {
+            if (editTextUsername.text.length >= 3 && editTextPassword.text.length >= 4) {
+                Log.d("LOGIN_FRAGMENT", "The username and password is valid")
+            } else {
+                showError()
+            }
+        }
+    }
+
+    private fun showError() {
+        val snackbar = Snackbar.make(button, "Username or password incorrect. Please try again", Snackbar.LENGTH_LONG)
+        snackbar.show()
     }
 }
