952c11c67f5d88c3ac2813f3e04d5f2b90badb41
Roberto Halgravez <r.halgravez@gmail.com>
2020-06-29 04:28:00 -0600

    Week 5 - Implementing the Parcelable interface in Movie class

--- Week5/Assignment5/app/src/main/java/com/raywenderlich/bootcamp/blockbusterrecyclerview/model/Movie.kt
+++ Week5/Assignment5/app/src/main/java/com/raywenderlich/bootcamp/blockbusterrecyclerview/model/Movie.kt
@@ -1,6 +1,9 @@
 package com.raywenderlich.bootcamp.blockbusterrecyclerview.model
 
+import android.os.Parcel
+import android.os.Parcelable
 import java.util.*
+
 
 class Movie(
     val id: String = UUID.randomUUID().toString(),
@@ -9,5 +12,38 @@ class Movie(
     val poster: Int,
     val rating: String,
     val summary: String
-) {
+) : Parcelable
+{
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readInt(),
+        parcel.readString()!!,
+        parcel.readString()!!
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(id)
+        parcel.writeString(title)
+        parcel.writeString(releaseDate)
+        parcel.writeInt(poster)
+        parcel.writeString(rating)
+        parcel.writeString(summary)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Movie> {
+        override fun createFromParcel(parcel: Parcel): Movie {
+            return Movie(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Movie?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
