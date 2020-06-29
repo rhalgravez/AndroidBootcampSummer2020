c52545ccde24e6100dfa3fec881cde7a8773ee69
Roberto Halgravez <r.halgravez@gmail.com>
2020-06-29 04:33:31 -0600

    Week 5 - Get the movie from the intent and show its content in the corresponding views in the DetailActivity

--- Week5/Assignment5/app/src/main/java/com/raywenderlich/bootcamp/blockbusterrecyclerview/DetailActivity.kt
+++ Week5/Assignment5/app/src/main/java/com/raywenderlich/bootcamp/blockbusterrecyclerview/DetailActivity.kt
@@ -2,10 +2,23 @@ package com.raywenderlich.bootcamp.blockbusterrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.raywenderlich.bootcamp.blockbusterrecyclerview.model.Movie
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
+
+    lateinit var movie: Movie
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        movie = intent.getParcelableExtra(MainFragment.INTENT_MOVIE_KEY) as Movie
+
+        movie_imageView.setImageResource(movie.poster)
+        title_textView.text = movie.title
+        releaseDate_textView.text = movie.releaseDate
+        rating_textView.text = "${movie.rating} / 10"
+        summary_textView.text = movie.summary
     }
 }
