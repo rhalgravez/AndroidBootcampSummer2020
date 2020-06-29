1b88d1af65b9b9caf2372667843c54107226823d
Roberto Halgravez <r.halgravez@gmail.com>
2020-06-29 01:38:13 -0600

    Week 5 - Add the menu in the MainFragment and handle the click events for the menu

--- Week5/Assignment5/app/src/main/java/com/raywenderlich/bootcamp/blockbusterrecyclerview/MainFragment.kt
+++ Week5/Assignment5/app/src/main/java/com/raywenderlich/bootcamp/blockbusterrecyclerview/MainFragment.kt
@@ -1,10 +1,9 @@
 package com.raywenderlich.bootcamp.blockbusterrecyclerview
 
 import android.os.Bundle
+import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.recyclerview.widget.GridLayoutManager
 import com.raywenderlich.bootcamp.blockbusterrecyclerview.model.MoviesDataSource
 import kotlinx.android.synthetic.main.fragment_main.*
@@ -22,6 +21,7 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
@@ -30,6 +30,19 @@ class MainFragment : Fragment() {
 
         recycler_view.layoutManager = GridLayoutManager(activity, 2)
         recycler_view.adapter = MoviesListAdapter(MoviesDataSource.getMoviesList())
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.log_out) {
+            Log.d("MAIN_FRAGMENT", "Do teh log out")
+        }
+        return true
 
     }
 
